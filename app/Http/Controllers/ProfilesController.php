5bf7a17454be412cda6ba7c04888f7ac3a210fc4
Aldar Pärnoja <aldar@optimistdigital.com>
2018-10-26 15:18:11 +0300

    Episode 27

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Activity;
 use App\User;
 use Illuminate\Http\Request;
 
@@ -15,19 +16,8 @@ class ProfilesController extends Controller
     {
         return view('profiles.show', [
             'profileUser' => $user,
-            'activities' => \App\Activity::feed($user)
+            'activities' => Activity::feed($user)
         ]);
-    }
-
-    /**
-     * @param User $user
-     * @return \Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Relations\HasMany[]|\Illuminate\Database\Query\Builder[]|\Illuminate\Support\Collection
-     */
-    protected function getActivity(User $user)
-    {
-        return $user->activity()->latest()->with('subject')->take(50)->get()->groupBy(function ($activity) {
-            return $activity->created_at->format('Y-m-d');
-        });
     }
 }
 
