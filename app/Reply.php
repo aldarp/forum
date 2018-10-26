8b2ef759ff9bfaf7d92465ebfb0aaf9871d783f8
Aldar Pärnoja <aldar@optimistdigital.com>
2018-10-26 13:37:41 +0300

    Episode 26

--- app/Reply.php
+++ app/Reply.php
@@ -25,5 +25,10 @@ class Reply extends Model
     {
         return $this->belongsTo(User::class, 'user_id');
     }
+
+    public function thread()
+    {
+        return $this->belongsTo(Thread::class);
+    }
 }
 
